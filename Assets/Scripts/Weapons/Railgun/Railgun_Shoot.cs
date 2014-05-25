279a53eac84f384f0b06dcb5d711d95a2c485dac
ngzaharias <applzor@gmail.com>
2014-05-25 22:29:03 +1000

    Added new files

---
+++ Assets/Scripts/Weapons/Railgun/Railgun_Shoot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Railgun_Shoot : _Weapon
+{
+
+    public GameObject[] particles;
+    public GameObject[] animations;
+    protected short attach = 0;
+
+    public override void Shoot()
+    {
+
+        if (timer >= cooldown)
+        {
+            timer = 0.0f;
+
+            //  Shoot the specific Weapon
+            //particles[attach].particleSystem.Play();
+            animations[attach].animation.Play();
+            DamageLine(animations[attach].transform);
+
+            //  Cycle attachments
+            attach++;
+            if (attach > animations.Length - 1)
+                attach = 0;
+        }
+    }
+
+    void DamageLine(Transform obj) {
+        //  Don't use raycast, will only hit the first object it comes across
+    }
+}
